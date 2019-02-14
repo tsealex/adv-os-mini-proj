b511565d0b6451398ae84ba20885bd9c14eda0b4
tsealex <atse@wisc.edu>
2019-02-13 22:22:36 -0600

    Finished benchmarks 1-4

--- configure.sh
+++ configure.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Generate the data file.
-truncate -s 2g /data/2GB.file
+truncate -s 2g /tmp/2GB.file
 # TODO: Restrict memory usage of a program https://thirld.com/blog/2012/02/09/things-to-remember-when-using-ulimit/
